286fa2223d5b95ad8355012b0c23e224f4376023
YehOkiHub <jeff077872@hotmail.com>
2023-06-18 20:17:08 -0400

    fixed up formatting

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
+      
       <Footer />
     </>
   );
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,52 +5,66 @@ const Resume = () => {
   return (
     <section className="resume">
       <div className="container">
-        <h1>Education and Certificates</h1>
+        <h1>Work</h1>
         <div className="resume__content">
           <div className="resume__item">
             <div className="resume__info">
               <div className="item1">
+                <h2>Teletech IT Support Specialist 2012-2019</h2>
+                  {/* <h3>Served customers from China and Taiwan in a High Paced environment for a large computer peripheral company. 
+                  Made sure hardware and software components were running smoothly </h3> */}
+
+                  <ul>
+                    <li> Interacted with an Average of 70 Customers a day</li>
+                    <li>Collaborated with Teams in China and Taiwan to provide a
+                  smoother customer experience.</li>
+                    <li>Boosted the average of the
+                  Team’s customer satisfaction surveys by at least 15%</li>
+                  <li>Mentored
+                  new hires in familiarizing themselves with salesforce as well
+                  as the company’s gaming software</li>
+
+                  </ul>
+                  
+                
+              </div>
+
+              <div className="item2">
                 <h2>Georgia Institute of Technology · Georgia</h2>
                 <p>Full-stack Coding Bootcamp: Feb 2023</p>
               </div>
 
-              <div className="item2">
+              <div className="item3">
                 <h2>Codeacademy · Online Course</h2>
                 <p>Javascript completion Certificate: Jan 2023</p>
               </div>
-
-              <div className="item3">
-                <h2>Skillcrush · Online Course</h2>
-
-                <p>Javascript completion Certificate: June 2023</p>
-              </div>
+              <div className="item4"></div>
             </div>
           </div>
           <div className="resume__item">
             <div className="resume__skills">
               <div>
-              <h2>Skills</h2>
+                <h2>Skills</h2>
               </div>
 
               <div>
-              <ul className="skill_list">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-                <li>SQL</li>
-                <li>MongoDB</li>
-                <li>GitHub</li>
-                <li>Express</li>
-                <li>React</li>
-                <li>Node</li>
-                <li>MVC</li>
-                <li>Handlebars</li>
-                <li>Bulma</li>
-                <li>Bootstrap</li>         
-              </ul>
+                <ul className="skill_list">
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Javascript</li>
+                  <li>SQL</li>
+                  <li>MongoDB</li>
+                  <li>GitHub</li>
+                  <li>Express</li>
+                  <li>React</li>
+                  <li>Node</li>
+                  <li>MVC</li>
+                  <li>Handlebars</li>
+                  <li>Bulma</li>
+                  <li>Bootstrap</li>
+                </ul>
               </div>
-
-            </div>            
+            </div>
           </div>
         </div>
       </div>
